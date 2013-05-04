3b413bd6472bb23e512ea3a2d0700df535e32d8b
Igor Artamonov <igor@artamonov.ru>
2013-05-04 19:29:10 +0700

    copy grails fb and twitter

---
+++ copy-fb.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf grails-spring-security-facebook/*
+cp -R ~/Projects/grails-dev/grails-spring-security-facebook/target/docs/* grails-spring-security-facebook/
---
+++ copy-twitter.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf grails-spring-security-twitter/*
+cp -R ~/Projects/grails-dev/grails-spring-security-twitter/target/docs/* grails-spring-security-twitter/
